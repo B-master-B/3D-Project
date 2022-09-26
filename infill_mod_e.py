0746413980d7c09e8d3b2122744c60be9b79b240
Abel Nagy <abel.nagy.m4tankor@gmail.com>
2022-09-26 15:39:43 +0200

    Solve the retraction problem.

--- infill_mod_e.py
+++ infill_mod_e.py
@@ -1,14 +1,19 @@
+# %%
 from gcodeparser import GcodeParser
 import re
+import matplotlib.pyplot as plt
 
 with open('1d_piskota_infill_cross.gcode', 'r') as f:
   gcode = f.read()
 lines = GcodeParser(gcode, True).lines
 
 inInfill = False
+afterRetraction = False
 
 prevE = 0
 prevEnew = 0
+
+Epars = []
 
 for line in lines:
     if line.command[0] == ";" and line.comment.startswith("@AreaBegin \"Infill\""):
@@ -19,23 +24,31 @@ for line in lines:
         continue
     if not inInfill:
         continue
-    if line.command_str == "G92":
+    if line.command_str == "G92": # set E parameter to the defined value
         prevE = line.params['E']
         prevEnew = line.params['E']
+        continue
 
     if line.command_str == 'G1':
         E = line.params['E']
         deltaE = E - prevE
         prevE = E
-        if deltaE < 0: # retraction -> we don't modify the E value
+        if deltaE < 0: # retraction -> we don't modify the deltaE value
+            afterRetraction = True
             deltaEnew = deltaE
-        else: # extrusion
-            deltaEnew = deltaE * p
+        else: # deltaE > 0
+            if afterRetraction: # after retraction -> we don't modify the deltaE value
+                afterRetraction = False
+                deltaEnew = deltaE
+            else: # extrusion -> we modify the deltaE value according to the p value
+                deltaEnew = deltaE * p
+        
         Enew = prevEnew + deltaEnew
         prevEnew = Enew
-
         line.update_param('E', Enew)
 
 with open('1d_piskota_infill_cross_mod.gcode', 'w') as f:
     for line in lines:
         f.write(f"{line.gcode_str}\n")
+
+# %%
