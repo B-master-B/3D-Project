3a384cea986d4bf88e1963b1e13383d7c16b86dc
Abel Nagy <abel.nagy.m4tankor@gmail.com>
2022-10-03 15:59:20 +0200

    Start to implement the segmentation.

--- infill_mod_e.py
+++ infill_mod_e.py
@@ -1,19 +1,25 @@
 # %%
 from gcodeparser import GcodeParser
 import re
+from copy import deepcopy
 import matplotlib.pyplot as plt
+
+
+# Parameters
+segmentLength = 1.0 # in [mm]
 
 with open('1d_piskota_infill_cross.gcode', 'r') as f:
   gcode = f.read()
 lines = GcodeParser(gcode, True).lines
 
 inInfill = False
-afterRetraction = False
+retraction = False
+printing = False
 
-prevE = 0
-prevEnew = 0
-
-Epars = []
+prevX = -9999.9
+prevY = -9999.9
+prevE = 0.0
+prevEnew = 0.0
 
 for line in lines:
     if line.command[0] == ";" and line.comment.startswith("@AreaBegin \"Infill\""):
@@ -34,18 +40,33 @@ for line in lines:
         deltaE = E - prevE
         prevE = E
         if deltaE < 0: # retraction -> we don't modify the deltaE value
-            afterRetraction = True
+            retraction = True
+            printing = False
             deltaEnew = deltaE
         else: # deltaE > 0
-            if afterRetraction: # after retraction -> we don't modify the deltaE value
-                afterRetraction = False
+            if retraction: # after retraction -> we don't modify the deltaE value
+                retraction = False
+                printing = False
                 deltaEnew = deltaE
             else: # extrusion -> we modify the deltaE value according to the p value
+                printing = True
                 deltaEnew = deltaE * p
         
-        Enew = prevEnew + deltaEnew
-        prevEnew = Enew
-        line.update_param('E', Enew)
+        if printing:
+            endX = line.params['X']
+            endY = line.params['Y']
+            # we have to implement segmentation here
+            # totalLength = norm([endX, endY] - [prevX, prevY])
+            # ...
+
+        else: # retraction or after retraction command
+            Enew = prevEnew + deltaEnew
+            prevEnew = Enew
+            line.update_param('E', round(Enew, 5))
+    
+    if line.command_str == 'G0' or line.command_str == 'G1':
+        prevX = line.params['X']
+        prevY = line.params['Y']
 
 with open('1d_piskota_infill_cross_mod.gcode', 'w') as f:
     for line in lines:
